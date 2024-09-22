2da3a973b4bbcc804a33062906624192addf2de0
zekeriyabasan <zekeriyabasan@gmail.com>
2024-09-22 18:37:05 +0300

    commit last branch

--- WebProgrammingWithREACT/northwind-redux/src/index.js
+++ WebProgrammingWithREACT/northwind-redux/src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './components/root/App';
 import reportWebVitals from './reportWebVitals';
 
+import { Provider } from 'react-redux';
+import storeConfigure from './redux/reducers/configureStore';
+
+const store = storeConfigure();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
---
+++ WebProgrammingWithREACT/northwind-redux/src/redux/reducers/storeConfigure.js
@@ -0,0 +1,18 @@
+import { configureStore } from '@reduxjs/toolkit'; // Redux Toolkit'i içe aktar
+import rootReducer from './index'; // Reducers'ınızı içe aktar
+
+// configureStore fonksiyonu
+const storeConfigure = () => {
+  return configureStore({
+    reducer: rootReducer, // Reducer'ları yapılandırmada kullanın
+    // Diğer yapılandırma seçeneklerini buraya ekleyebilirsiniz
+    // Örneğin:
+    // middleware: (getDefaultMiddleware) =>
+    //   getDefaultMiddleware({
+    //     serializableCheck: false, // Özellikle API çağrıları için devtools uyumluluğunu devre dışı bırakın
+    //   }),
+    // devTools: process.env.NODE_ENV !== 'production', // Geliştirme ortamında devtools'u etkinleştirin
+  });
+};
+
+export default storeConfi;
