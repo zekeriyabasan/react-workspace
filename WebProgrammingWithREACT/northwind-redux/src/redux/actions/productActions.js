4f830a3ab17148187a06e2cb2e155c2552cc7eaa
zekeriyabasan <zekeriyabasan@gmail.com>
2024-10-19 16:18:24 +0300

    feat: add or update product actions and handle exception

--- WebProgrammingWithREACT/northwind-redux/src/redux/actions/productActions.js
+++ WebProgrammingWithREACT/northwind-redux/src/redux/actions/productActions.js
@@ -1,19 +1,58 @@
 import * as actionTypes from './actionTypes'
 
+
+export function createProductSuccess(product) {
+    return { type: actionTypes.CREATE_PRODUCT_SUCCESS, payload: product }
+}
+export function updateProductSuccess(product) {
+    return { type: actionTypes.UPDATE_PRODUCT_SUCCESS, payload: product }
+}
+
+export function saveProductApi(product) {
+    return fetch("http://localhost:3000/products/" + (product.id || ""), {
+        method: product.id ? "PUT" : "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify(product)
+
+    }).then(handleResponse).catch(handleError)
+}
+
+export function saveProduct(product){
+    return function(dispatch){
+        return saveProductApi(product).then(saveProduct => {
+            product.id? dispatch(updateProductSuccess(saveProduct)) : dispatch(createProductSuccess(saveProduct))
+        }).catch(error => {
+            throw error;
+        });
+    }
+}
+
+export async function handleResponse(response) {
+    if(response.ok){
+        return response.json()
+    }
+    const error = await response.text()
+    throw new Error(error)
+}
+export function handleError(error){
+    console.log(error + "bir hata oluştu")
+    throw error
+}
+
 // return products as redux {}
-export function getProductsSuccess(products){
-    return {type:actionTypes.GET_PRODUCTS_SUCCESS, payload: products}
+export function getProductsSuccess(products) {
+    return { type: actionTypes.GET_PRODUCTS_SUCCESS, payload: products }
 }
 // return a function for getProductsSuccess can prepare for redux {}
-export function getProducts(categoryId){
-    return function(dispatch) {
+export function getProducts(categoryId) {
+    return function (dispatch) {
         let url = "http://localhost:3000/products";
-        if(categoryId)
+        if (categoryId)
             url = url + "?categoryId=" + categoryId;
 
-        
-        return fetch(url).then(response=>response.json()).
-        then(result=>dispatch(getProductsSuccess(result)))
+
+        return fetch(url).then(response => response.json()).
+            then(result => dispatch(getProductsSuccess(result)))
 
 
 
--- WebProgrammingWithREACT/northwind-redux/src/redux/reducers/index.js
+++ WebProgrammingWithREACT/northwind-redux/src/redux/reducers/index.js
@@ -3,11 +3,13 @@ import changeCategoryReducer from './changeCategoryReducer'
 import categoryListReducer from './categoryListReducer'
 import productListReducer from './productListReducer'
 import cartReducer from "./cartReducer";
+import saveProductReducer from "./saveProductReducer";
 const rootReducer = combineReducers({
     changeCategoryReducer,
     categoryListReducer,
     productListReducer,
-    cartReducer
+    cartReducer,
+    saveProductReducer
 })
 
 export default rootReducer;
